0a5af49dd16897153290a851dede86b76881c387
ChrisKnowsDev <chrisharley81@gmail.com>
2021-07-02 07:08:52 -0400

    init

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is your local neighborhood event directory</p>
+      <p>Version 1.0.0</p>
+    </div>
+  )
+}
+
---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+  return (
+    <div>
+      <h1>my event</h1>
+    </div>
+  )
+}
