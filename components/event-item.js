06941ac269b3642bfb016468d42d79a77d75be3e
ChrisKnowsDev <chrisharley81@gmail.com>
2021-07-04 09:13:12 -0400

    strapi backend setup and sourcing data in frontend

--- components/event-item.js
+++ components/event-item.js
@@ -7,7 +7,11 @@ export default function EventItem({ evt }) {
     <div className={styles.event}>
       <div className={styles.img}>
         <Image
-          src={evt.image ? evt.image : '/images/event-default.jpg'}
+          src={
+            evt.image
+              ? evt.image.formats.thumbnail.url
+              : '/images/event-default.jpg'
+          }
           width={170}
           height={100}
           alt="event-image"
@@ -16,7 +20,7 @@ export default function EventItem({ evt }) {
 
       <div className={styles.info}>
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
         <h3>{evt.title}</h3>
       </div>
