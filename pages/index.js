11d1b6a75c263f6ac6adab6797e95de2c4854f51
ChrisKnowsDev <chrisharley81@gmail.com>
2021-07-09 14:13:07 -0400

    edited register link text on signup page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
+import Link from 'next/link'
 import Layout from '@/components/layout'
 import EventItem from '@/components/event-item'
 import { API_URL } from '@/config/index'
-import Link from 'next/link'
 
 export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
-      {events.length === 0 && <h3>No Events To Show</h3>}
+      {events.length === 0 && <h3>No events to show</h3>}
 
-      {events.map(evt => {
-        return <EventItem key={evt.id} evt={evt} />
-      })}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
 
       {events.length > 0 && (
         <Link href="/events">
-          <a className="btn-secondary">All Events</a>
+          <a className="btn-secondary">View All Events</a>
         </Link>
       )}
     </Layout>
